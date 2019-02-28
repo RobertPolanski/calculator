75b04a7120ce755a819edda71c0226bc84fdf37a
Robert Polanski <rpolanskii@gmail.com>
2019-02-28 15:05:25 +0100

    .

--- src/test/java/com/polanski/calculator/CalculatorTest.java
+++ src/test/java/com/polanski/calculator/CalculatorTest.java
@@ -6,11 +6,12 @@ import static org.junit.Assert.assertEquals;
  * Calculator test.d
  */
 public class CalculatorTest {
+
     private Calculator calculator = new Calculator();
 
     @Test
     public void testSum() {
-        assertEquals(5, calculator.sum(2, 3));
+        assertEquals(6, calculator.sum(3, 3));
     }
 }
 
