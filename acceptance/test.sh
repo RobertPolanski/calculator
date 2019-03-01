5b097de281ee3b8e09c5e46255fd8198b175e009
Robert Polanski <rpolanskii@gmail.com>
2019-03-01 18:25:27 +0100

    .

--- acceptance/test.sh
+++ acceptance/test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
  sleep 20
-test $(curl calculator:8080/sum?a=1\&b=2) -eq 3
+CALCULATOR_PORT=$(docker-compose port calculator 8080 | cut -d: -f2)
+test $(curl calculator:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
